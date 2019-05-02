68583370cadbb23a8ee76a2a1856dc82cd5cccef
dashtirko <dashtihussein95@gmail.com>
2019-05-02 02:06:34 -0700

    Project

--- App.js
+++ App.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
-import { StyleSheet, Text, View } from 'react-native';
-import Todo from './components/Todo';
+import { StyleSheet,ScrollView,Image, Text, View } from 'react-native';
+//import Todo from './components/Todo';
 
 export default class App extends React.Component {
   render() {
     return (
-      <PaperProvider>
+       
         <View style={styles.container}>
-          <Todo />
+        <View style={{marginTop:35 ,alignItems:"center"}}>
+      <ScrollView> 
+          <View> 
+                   <Image
+          style={{width: 250, height: 100}}
+          source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}
+        />
+        <Text>This  is  info  about  Image ...</Text>
+          </View>
+
+          <View> 
+                   <Image
+          style={{width: 250, height: 100}}
+          source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}
+        />
+        <Text>This  is  info  about  Image  2...</Text>
+          </View>
+
+
+          <View> 
+                   <Image
+        style={{width: 250, height: 100}}
+          source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}
+        />
+        <Text>This  is  info  about  Image  3...</Text>
+          </View>
+    </ScrollView>
+    </View>
         </View>
-      </PaperProvider>
+      
     );
   }
 }
